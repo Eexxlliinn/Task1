530506fcc0be976796a8ab1d4de45ca95bb6f8dd
Eugene Yuntsevich <uncevichzena@gmail.com>
2024-11-20 18:39:44 +0300

    MS-2 Update Migration Manager to work with versions of database and rollbacks, add rollback scripts

--- src/main/java/me/eexxlliinn/service/impl/MigrationManagerImpl.java
+++ src/main/java/me/eexxlliinn/service/impl/MigrationManagerImpl.java
@@ -1,17 +1,16 @@
 package me.eexxlliinn.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import me.eexxlliinn.connection.ConnectionManager;
+import me.eexxlliinn.entities.Migration;
 import me.eexxlliinn.executor.impl.SqlExecutorImpl;
 import me.eexxlliinn.filereader.impl.FileReaderImpl;
 import me.eexxlliinn.service.MigrationManager;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.Set;
+import java.sql.*;
+import java.util.*;
 
+@Slf4j
 public class MigrationManagerImpl implements MigrationManager {
 
     private final FileReaderImpl fileReader;
@@ -25,42 +24,119 @@ public class MigrationManagerImpl implements MigrationManager {
     }
 
     @Override
-    public void runMigrations(String changelogPath) throws Exception {
-        Set<String> appliedMigrations = getAppliedMigrations();
-        Set<String> availableMigrations = sqlExecutor.parseChangelog(changelogPath);
+    public void runMigrationsToVersion(String changelogPath, String targetVersion) throws Exception {
+        log.info("Running migrations to version {}", targetVersion);
+        List<Migration> availableMigrations = sqlExecutor.parseChangelog(changelogPath);
+        List<Migration> appliedMigrations = getAppliedMigrations();
+        connection.setAutoCommit(false);
+        try {
+            for (Migration migration : availableMigrations) {
+                String version = migration.getVersion();
+                if (compareVersions(version, targetVersion) > 0) {
+                    break;
+                }
+                boolean alreadyApplied = appliedMigrations.contains(migration);
 
-        for (String migration : availableMigrations) {
-            if (!appliedMigrations.contains(migration)) {
-                sqlExecutor.runMigration(connection, migration);
-                recordMigration(migration);
-            } else {
-                //log.info();
+                if (!alreadyApplied) {
+                    sqlExecutor.runMigration(connection, migration.getFile(), migration.getVersion());
+                    recordMigration(version, migration.getFile(), migration.getRollbackFile());
+                } else {
+                    log.info("Skipping {} with version {}", migration, version);
+                }
             }
-
+            connection.commit();
+            log.info("Migrations applied to version {}", targetVersion);
+        } catch (Exception e) {
+            connection.rollback();
+            log.error("Migrations applied to version {} failed", targetVersion);
+            throw e;
+        } finally {
+            connection.setAutoCommit(true);
         }
+
     }
 
     @Override
-    public Set<String> getAppliedMigrations() {
-        Set<String> migrations = new HashSet<>();
-        String query = "SELECT file_name FROM migration_history";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
+    public List<Migration> getAppliedMigrations() {
+        List<Migration> appliedMigrations = new ArrayList<>();
+        String query = "SELECT version, file_name, rollback FROM migration_history ORDER BY applied_at ASC";
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(query);
             while (resultSet.next()) {
-                migrations.add(resultSet.getString("file_name"));
+                String version = resultSet.getString("version");
+                String fileName = resultSet.getString("file_name");
+                String rollback = resultSet.getString("rollback");
+                appliedMigrations.add(new Migration(version, fileName, rollback));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return migrations;
+        return appliedMigrations;
     }
 
     @Override
-    public void recordMigration(String appliedMigration) throws SQLException {
-        String query = "INSERT INTO migration_history (file_name) VALUES (?)";
+    public void rollbackMigration(String changelogPath, String targetVersion) throws Exception {
+        log.info("Rolling back migration to version {}", targetVersion);
+        List<Migration> appliedMigrations = getAppliedMigrations();
+        List<Migration> appliedList = appliedMigrations.stream()
+                        .sorted((m1, m2) -> compareVersions(m2.getVersion(), m1.getVersion()))
+                        .toList();
+        connection.setAutoCommit(false);
+        try {
+            for (Migration migration : appliedList) {
+                String version = migration.getVersion();
+                if (compareVersions(version, targetVersion) <= 0) {
+                    break;
+                }
+                sqlExecutor.runMigration(connection, migration.getRollbackFile(), migration.getVersion());
+                removeMigrationRecord(migration.getFile());
+            }
+            connection.commit();
+            log.info("Rollback migrations applied to version {}", targetVersion);
+        } catch (Exception e) {
+            connection.rollback();
+            log.info("Rollback migrations applied to version {} failed", targetVersion);
+            throw e;
+        }
+    }
+
+    @Override
+    public void recordMigration(String version, String appliedMigration, String rollbackFile) throws SQLException {
+        log.info("Recording migration {}", appliedMigration);
+        String query = "INSERT INTO migration_history (version, file_name, rollback, applied_at) VALUES (?, ?, ?, CURRENT_TIMESTAMP)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, version);
+            preparedStatement.setString(2, appliedMigration);
+            preparedStatement.setString(3, rollbackFile);
+            preparedStatement.execute();
+        }
+    }
+
+    @Override
+    public void removeMigrationRecord(String appliedMigration) throws SQLException {
+        log.info("Removing migration {}", appliedMigration);
+        String query = "DELETE FROM migration_history WHERE file_name = ?";
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setString(1, appliedMigration);
             preparedStatement.execute();
         }
+    }
+
+    private int compareVersions(String version1, String version2) {
+        String[] parts1 = version1.split("\\.");
+        String[] parts2 = version2.split("\\.");
+
+        int length = Math.max(parts1.length, parts2.length);
+
+        for (int i = 0; i < length; i++) {
+            int part1 = Integer.parseInt(parts1[i]);
+            int part2 = Integer.parseInt(parts2[i]);
+            if (part1 > part2) {
+                return 1;
+            } else if (part1 < part2) {
+                return -1;
+            }
+        }
+        return 0;
     }
 }
--- src/main/resources/db/changelog/create-migration-history-table.sql
+++ src/main/resources/db/changelog/create-migration-history-table.sql
@@ -1,4 +1,7 @@
 CREATE TABLE IF NOT EXISTS migration_history (
     id SERIAL PRIMARY KEY,
-    file_name VARCHAR(255) NOT NULL
+    version VARCHAR(255) NOT NULL,
+    file_name VARCHAR(255) NOT NULL,
+    rollback VARCHAR(255) NOT NULL,
+    applied_at TIMESTAMP
 );
